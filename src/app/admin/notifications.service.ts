bb69e94d7d4ccf9de37ad83daaf80bb07bfd3af6
nader medhet <nadermedhet142@gmail.com>
2020-04-30 15:09:26 +0200

    fix send notification

--- src/app/admin/notifications.service.ts
+++ src/app/admin/notifications.service.ts
@@ -2,51 +2,54 @@ import { Injectable } from '@angular/core';
 
 import { AdminService } from "../admin/admin.service";
 
-import { HttpClient,HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationsService {
-  tockens=[];
+  tockens = [];
 
   constructor(
-    private admin :AdminService,
+    private admin: AdminService,
     private httpClient: HttpClient
   ) { }
-  
-  async notifications( title , content){
-    let  headers;
+
+  async notifications(title, content) {
+    let headers;
     let body;
-  this.tockens =[]
-   let key='AAAAoXhq9Ag:APA91bFIrZ6Q5rGO5xRwgLP7yGzBSJ0uEueJ_NLhALl54Af_BBo7GXgVpIAuLaFe1Ydfso7TX-mcRetpI97Ws5gigwVrfQ9R3q34kCsQgB3ZJ9h9EReFmr-fFaxS3FY200jmg4W9oTm5'
-    this.admin.getAllUser().subscribe(e=>{
-     // console.log('sada',e)
-      e.forEach(element => {
-       
-       if (element.token){
-        this.tockens.push(element.token)
-       } 
-      
+    this.tockens = []
+    let key = 'key=AAAAoXhq9Ag:APA91bFIrZ6Q5rGO5xRwgLP7yGzBSJ0uEueJ_NLhALl54Af_BBo7GXgVpIAuLaFe1Ydfso7TX-mcRetpI97Ws5gigwVrfQ9R3q34kCsQgB3ZJ9h9EReFmr-fFaxS3FY200jmg4W9oTm5'
+    this.admin.getAllUser().subscribe(e => {
+      // console.log('sada',e)
+      e.forEach((element: any) => {
+
+        if (element.token) {
+          this.tockens.push(element.token)
+        }
+
       });
-      console.log('tock',this.tockens)
-      const options ={
-     headers: new HttpHeaders({
-      'Authorization': key , 
-          'registration_ids' :this.tockens
-    })
-  }
-   body={ "notification": {
-      "title": title, 
-      "body": content
+      console.log('tock', this.tockens)
+      const options = {
+        headers: new HttpHeaders({
+          'Authorization': key,
+        })
       }
-    }
-    return ( this.httpClient.post('Https://fcm.googleapis.com/fcm/send',body,options).toPromise().then(res=>{
-      console.log(res)
+      body = {
+        "notification": {
+          "title": title,
+          "body": content,
+
+        },
+        'registration_ids': this.tockens
+
+      }
+      return (this.httpClient.post('Https://fcm.googleapis.com/fcm/send', body, options).toPromise().then(res => {
+        console.log(res)
+      })
+      )
     })
-  )
-    })
-  //  return (<any>await this.httpClient.post(`Https://fcm.googleapis.com/fcm/send`,body,headers)).toPromise()
-  
+    //  return (<any>await this.httpClient.post(`Https://fcm.googleapis.com/fcm/send`,body,headers)).toPromise()
+
   }
   // return (<any>await this.httpClient.post(`Https://fcm.googleapis.com/fcm/send`+key,{
   //   body: body,
